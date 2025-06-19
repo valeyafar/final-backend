a2a0766be6854d69790441e89f970e1698530ed8
valeyafar <valeeyafar@gmail.com>
2025-06-19 18:00:34 -0300

    Docummentacion de la API con Swagger agregado

---
+++ product/src/main/java/com/ucc/product/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.ucc.product.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Productos - UCC")
+                        .version("1.0")
+                        .description("Documentación de la API para productos.")
+                        .contact(new Contact()
+                                .name("Valentina Yafar")
+                                .email("yafarvalentina@gmail.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")));
+    }
+}
